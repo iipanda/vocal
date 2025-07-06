4c71e21453ef35b71f7ec844bfcafa97d2a74197
Karol Chudzik <iipandarx@protonmail.com>
2025-07-06 16:49:19 +0200

    refactor: remove legacy CSS styles and add default icon to tray menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -129,6 +129,7 @@ pub fn run() {
             
             // Create tray icon
             let _tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
                 .show_menu_on_left_click(false)
                 .on_menu_event(move |app, event| {
